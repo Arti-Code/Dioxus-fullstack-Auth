f3cf7f00c6bdfb0d2c87ca127caccf3bc20c3b6e
Arturo <41492101+Arti-Code@users.noreply.github.com>
2025-06-28 10:02:54 +0200

    v0.2.2

--- src/components/login.rs
+++ src/components/login.rs
@@ -10,8 +10,8 @@ pub fn Login() -> Element {
   let navigator = use_navigator();
   let mut user_session = use_context::<Signal<UserSession>>();
   rsx!(
-    div { class: "w-2/3 flex justify-center items-center bg-slate-800",
-      div { class: "border-solid border-2 border-slate-700 rounded-lg px-3 py-5 w-1/4",
+    div { class: "w-2/3 flex justify-center items-center bg-slate-800 mx-auto my-5",
+      div { class: "border-solid justify-center items-center border-2 border-slate-700 rounded-lg px-3 py-5 w-full",
         div { class: "text-center text-3xl", "Login" }
         if !error_msg.to_string().is_empty() {
           div { class: "bg-rose-100 text-rose-600 py-1 px-2 rounded-lg my-3",
@@ -21,7 +21,7 @@ pub fn Login() -> Element {
         div { class: "my-5",
           div { class: "text-lg text-slate-300", "username: " }
           input {
-            class: "w-full rounded-lg px-2 py-1",
+            class: "w-full rounded-lg px-2 py-1 bg-slate-100",
             r#type: "text",
             value: username,
             oninput: move |e| username.set(e.value()),
@@ -30,14 +30,14 @@ pub fn Login() -> Element {
         div { class: "my-5",
           div { class: "text-lg text-slate-300", "password: " }
           input {
-            class: "w-full rounded-lg px-2 py-1",
+            class: "w-full rounded-lg px-2 py-1 bg-slate-100",
             r#type: "password",
             value: password,
             oninput: move |e| password.set(e.value()),
           }
         }
         button {
-          class: "bg-sky-500 text-slate-20 px-3 py-2 rounded-lg w-full my-5 hover:bg-sky-600",
+          class: "flex bg-sky-500 text-slate-200 px-3 py-2 text-bold text-2xl rounded-lg w-1/2 mx-auto my-5 hover:bg-sky-600",
           onclick: move |_| async move {
               match log_in(username(), password()).await {
                   Ok(_) => {
--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -5,76 +5,79 @@ use crate::{Route, UserSession};
 #[component]
 pub fn Navbar() -> Element {
   let navigator = use_navigator();
-  //let mut session = use_context::<Signal<MySession>>();
-  //let client = use_context::<Signal<reqwest::Client>>();
-  //let s = session.read().clone();
-  let user_session = use_context::<Signal<UserSession>>();
+  let mut user_session = use_context::<Signal<UserSession>>();
   rsx! (
     nav { class:"bg-slate-900",
-          div { class:"flex w-2/3 justify-center",
-                ul {
-                  class:"flex row justify-left align-center w-full",
-                  if !user_session.read().is_logged() {
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700",
-                        onclick: move |_| {
-                          //session.write().logout();
-                          navigator.push(Route::Register{});
-                          
-                        }, 
-                        "REGISTER" 
-                      }
-                    }
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700",
-                          onclick: move |_| {
-                            //session.write().logout();
-                            navigator.push(Route::Login{});
-                          }, 
-                          "LOGIN" 
-                      }
-                    }
-                  } else {
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700",
-                        //class:"border-solid border-2 bg-slate-300 rounded-md px-3 py-2 text-sm font-medium text-gray-400 hover:bg-slate-700 hover:text-white",
-                        onclick: move |_| {
-                          //session.write().logout();
-                          navigator.replace(Route::Home{});
-                        }, 
-                        "HOME"
-                      } 
-                    }
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700",
-                        "ROBOTS" 
-                      }
-                    }
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700",
-                        "PROFILE"
-                      } 
-                    }
-                    li {
-                      class: "flex",
-                      button { 
-                        class: "bg-slate-800 mx-8 my-3 px-6 py-3 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700", 
-                        "LOGOUT" 
-                      }
-                    }
-                  }
-                }
+      div { class:"flex w-2/3 justify-center",
+        ul {
+          class:"flex row justify-left align-center w-full",
+          if !user_session.read().is_logged() {
+            li {
+              class: "flex",
+              button { 
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-blue-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700 hover:border-blue-500",
+                onclick: move |_| {
+                  //session.write().logout();
+                  navigator.push(Route::Register{});
+                  
+                }, 
+                "REGISTER" 
+              }
             }
+            li {
+              class: "flex",
+              button { 
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-green-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700 hover:border-green-500",
+                  onclick: move |_| {
+                    //session.write().logout();
+                    navigator.push(Route::Login{});
+                  }, 
+                  "LOGIN" 
+              }
+            }
+          } else {
+            li {
+              class: "flex",
+              button { 
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-slate-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700 hover:border-slate-300",
+                onclick: move |_| {
+                  //session.write().logout();
+                  navigator.replace(Route::Home{});
+                }, 
+                "HOME"
+              } 
+            }
+            li {
+              class: "flex",
+              button {
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-cyan-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-cyan-700 hover:border-cyan-500",
+                "ROBOTS" 
+              }
+            }
+            li {
+              class: "flex",
+              button { 
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-cyan-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700 hover:border-cyan-500",
+                "PROFILE"
+              } 
+            }
+            li {
+              class: "flex",
+              button { 
+                class: "bg-slate-800 mx-8 my-3 px-6 py-3 border-orange-400 border-solid border-2 rounded-xl text-center text-bold text-2xl text-slate-200 hover:bg-slate-700 hover:border-orange-500",
+                onclick: move |_| {
+                  let s = user_session.write().clone();
+                  if s.is_logged() {
+                    user_session.write().logout();
+                    navigator.replace(Route::Home{});
+                  }
+                },
+                "LOGOUT" 
+              }
+            }
+          }
+        }
+      }
     }
     Outlet::<Route> {}
   )
